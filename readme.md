4c71c4aa2b72b8f7849b4d35573c9cdcea84717c
Galaxy13 <gen.hort54@gmail.com>
2024-02-27 16:38:00 +0300

    Update: initial commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+## Otus Java Developer Repository
---
+++ src/main/java/com/galaxy13/Main.java
@@ -0,0 +1,7 @@
+package com.galaxy13;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
