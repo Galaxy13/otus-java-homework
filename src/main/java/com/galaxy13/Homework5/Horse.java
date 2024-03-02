b4875c3e3c058c3ca1a78fd9ec2b64b231105a5f
Galaxy13 <gen.hort54@gmail.com>
2024-03-02 15:27:27 +0300

    Update: initial commit of Homework5

---
+++ src/main/java/com/galaxy13/Homework5/Horse.java
@@ -0,0 +1,7 @@
+package com.galaxy13.Homework5;
+
+public class Horse extends Animal {
+    public Horse(String name, double runningVelocity, double swimmingVelocity, double endurance) {
+        super(name, runningVelocity, swimmingVelocity, endurance, 4);
+    }
+}
--- src/main/java/com/galaxy13/Main.java
+++ src/main/java/com/galaxy13/Main.java
@@ -1,7 +1,29 @@
 package com.galaxy13;
 
+import com.galaxy13.Homework5.Cat;
+import com.galaxy13.Homework5.Dog;
+import com.galaxy13.Homework5.Horse;
+
 public class Main {
+    private static void printTime(double time) {
+        if (time != -1) {
+            System.out.printf("Time spent: %s s%n", time);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Horse horse = new Horse("Pony", 10.0, 2.0, 50);
+        printTime(horse.run(10));
+        printTime(horse.swim(30));
+        horse.info();
+
+        Cat cat = new Cat("Walky", 5.0, 0.0, 100);
+        printTime(cat.run(90));
+        printTime(cat.swim(120));
+        cat.info();
+
+        Dog dog = new Dog("Bobik", 20.0, 5.0, 150);
+        printTime(dog.swim(5));
+        printTime(dog.run(60));
+        dog.info();
     }
 }
