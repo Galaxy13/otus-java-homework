c12a9824d40e2a3dbfd50cb40d98dede645dfd2f
Galaxy13 <gen.hort54@gmail.com>
2024-03-06 14:44:41 +0300

    Update: initial commit of Homework9

---
+++ src/main/java/com/galaxy13/Homework9/Homework9.java
@@ -0,0 +1,50 @@
+package com.galaxy13.Homework9;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Homework9 {
+    public static ArrayList<Integer> range(int min, int max) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = min; i <= max; i++) {
+            arrayList.add(i);
+        }
+        return arrayList;
+    }
+
+    public static int sumOfElementsBiggerFive(List<Integer> integerList) {
+        int sumBiggerFive = 0;
+        for (int number : integerList) {
+            if (number > 5) {
+                sumBiggerFive += 5;
+            }
+        }
+        return sumBiggerFive;
+    }
+
+    public static void fillList(int value, List<Object> fillingList) {
+        Collections.fill(fillingList, value);
+    }
+
+    public static void incrementValue(int value, List<Integer> integerList) {
+        integerList.replaceAll(number -> number + value);
+    }
+
+    public static List<String> getNamesList(List<User> userList) {
+        return userList.stream().map(User::getName).collect(Collectors.toList());
+    }
+
+    public static List<User> getUserOlder(List<User> userList, int minAge) {
+        return userList.stream().filter(user -> user.getAge() >= minAge).collect(Collectors.toList());
+    }
+
+    public static boolean checkAvgAge(List<User> userList, int averageAge) {
+        return userList.stream().map(User::getAge).reduce(Integer::sum).orElse(0) / (double) averageAge > averageAge;
+    }
+
+    public static User youngestUser(List<User> userList) {
+        return userList.stream().min(User::compareTo).orElse(null);
+    }
+}
--- src/main/java/com/galaxy13/Main.java
+++ src/main/java/com/galaxy13/Main.java
@@ -1,7 +1,30 @@
 package com.galaxy13;
 
+import com.galaxy13.Homework9.Homework9;
+import com.galaxy13.Homework9.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Homework9.range(1, 5));
+
+        List<Integer> intList1 = List.of(1, 6, 10, 5, -1);
+        System.out.println(Homework9.sumOfElementsBiggerFive(intList1));
+
+        List<Object> testList2 = new ArrayList<>(List.of("well", 234, 56.0, List.of()));
+        Homework9.fillList(23, testList2);
+        System.out.println(testList2);
+
+        List<Integer> testList3 = new ArrayList<>(List.of(1, 10, 100, 0));
+        Homework9.incrementValue(2, testList3);
+        System.out.println(testList3);
+
+        List<User> testList4 = List.of(new User("John", 23), new User("Martha", 46), new User("Mary", 20), new User("Gennadiy", 24));
+        System.out.println(Homework9.getNamesList(testList4));
+        System.out.println(Homework9.getUserOlder(testList4, 24));
+        System.out.println(Homework9.checkAvgAge(testList4, 33));
+        System.out.println(Homework9.youngestUser(testList4));
     }
 }
