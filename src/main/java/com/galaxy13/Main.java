e05a346e41c41632367bbd58e0668086c528dea7
Galaxy13 <gen.hort54@gmail.com>
2024-03-06 16:48:47 +0300

    Update: initial commit of Homework10

--- src/main/java/com/galaxy13/Main.java
+++ src/main/java/com/galaxy13/Main.java
@@ -1,7 +1,24 @@
 package com.galaxy13;
 
+import com.galaxy13.Homework10.PhoneBook;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("+7923478", "Alexandr Ivanov");
+        phoneBook.find("Alexandr Ivanov");
+        phoneBook.find("Someone");
+
+        phoneBook.add("+9999", "Alexey Frolov");
+        phoneBook.add("71823740", "Alexey Frolov");
+        phoneBook.find("Alexey Frolov");
+
+        System.out.println(phoneBook.containsPhoneNumber("+99999"));
+        System.out.println(phoneBook.containsPhoneNumber("0000"));
+
+        phoneBook.add(null, null);
+        phoneBook.find(null);
+
+        phoneBook.find("aff dgr tafj adioi ee");
     }
 }
