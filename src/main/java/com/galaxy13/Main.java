adb583f44bcd72c3097421ae4e987b77733b0d7d
Galaxy13 <gen.hort54@gmail.com>
2024-03-25 14:31:17 +0300

    Update: sorting check added

--- src/main/java/com/galaxy13/Main.java
+++ src/main/java/com/galaxy13/Main.java
@@ -1,6 +1,7 @@
 package com.galaxy13;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -19,12 +20,30 @@ public class Main {
         }
         return list;
     }
+
+    private static boolean checkIfSorted(List<Integer> integerList) {
+        if (integerList == null || integerList.size() == 1) {
+            return true;
+        }
+
+        Iterator<Integer> iterator = integerList.iterator();
+        Integer current;
+        Integer previous = iterator.next();
+        while (iterator.hasNext()) {
+            current = iterator.next();
+            if (previous.compareTo(current) > 0) {
+                return false;
+            }
+            previous = current;
+        }
+        return true;
+    }
     public static void main(String[] args) {
         List<Integer> testList1 = generateRandomIntegerList(10_000_000, 100_000);
         outDuration(MultiThreadMerge.mergeSortMultiThread(testList1), "multi");
-//        System.out.println(testList1);
+        System.out.println("List is sorted: " + checkIfSorted(testList1));
         List<Integer> testList2 = generateRandomIntegerList(10_000_000, 100_000);
         outDuration(MultiThreadMerge.mergeSortSingleThread(testList2), "single");
-//        System.out.println(testList2);
+        System.out.println("List is sorted: " + checkIfSorted(testList2));
     }
 }
